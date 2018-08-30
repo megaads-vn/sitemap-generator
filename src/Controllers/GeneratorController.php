43c038eac7d2ad20f9d9dd5c7799d1df4896e7de
Bach Nguyen <bachnx2303@gmail.com>
2018-08-30 11:50:28 +0700

    Change to default name

--- src/Controllers/GeneratorController.php
+++ src/Controllers/GeneratorController.php
@@ -3,7 +3,7 @@ namespace Megaads\Generatesitemap\Controller;
 
 use Megaads\Generatesitemap\Models\Store;
 
-class SitemapGeneratorController
+class GeneratorController
 {
     /**
      * Generate sitemap.xml from database tables
