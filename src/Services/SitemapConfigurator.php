b214700c816a0df15ad07b351d387d92d7de75e4
bachnx23 <bachnx2303@gmail.com>
2019-04-03 14:25:34 +0700

    Update SitemapConfigurator.php

--- src/Services/SitemapConfigurator.php
+++ src/Services/SitemapConfigurator.php
@@ -5,9 +5,10 @@ namespace Megaads\Generatesitemap\Services;
 class SitemapConfigurator
 {
     protected $xmlString = "";
-    protected $defaultUrlSet = '<url><loc>#url</loc><priority>#piority</priority>#lastMode#changefreq</url>';
+    protected $defaultUrlSet = '<url><loc>#url</loc><priority>#piority</priority>#lastMode#changefreq#imageString</url>';
     protected $defaultLastMode = '<lastmod>#lastMode</lastmod>';
     protected $defaultChangefreq = '<changefreq>#changefreq</changefreq>';
+    protected $imageString = '<image:image><image:loc>#imagePath</image:loc><image:title><![CDATA[#imageTitte]]></image:title><image:caption><![CDATA[#imageCaption]]></image:caption></image:image>';
     protected $arrayUrlSet = [];
     protected $publicPath = null;
 
@@ -26,7 +27,7 @@ class SitemapConfigurator
      * @param $lastMode
      * @param $changefreq
      */
-    public function add($url, $piority, $lastMode='', $changefreq='')
+    public function add($url, $piority, $lastMode='', $changefreq='', $imageGallerys = [])
     {
         $stringUrlSet = $this->defaultUrlSet;
         $stringUrlSet = str_replace('#url', $url, $stringUrlSet);
@@ -46,6 +47,21 @@ class SitemapConfigurator
         }
 
         $stringUrlSet = str_replace('#changefreq', $changefreq, $stringUrlSet);
+
+        if (!empty($imageGallerys)) {
+            $str = '';
+            foreach($imageGallerys as $image) {
+                $imageString = $this->imageString;
+                foreach(array_keys($image) as $value) {
+                    $imageString = str_replace($value, $image[$value], $imageString);
+                }
+                $str .= $imageString;
+            }
+            $stringUrlSet = str_replace('#imageString', $str, $stringUrlSet);
+        } else {
+            $stringUrlSet = str_replace('#imageString', '', $stringUrlSet);
+        }
+
         array_push($this->arrayUrlSet, $stringUrlSet);
     }
 
@@ -120,7 +136,7 @@ class SitemapConfigurator
      */
     protected function addXmlHead() {
         $this->xmlString = '<?xml version="1.0" encoding="UTF-8"?>';
-        $this->xmlString .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">';
+        $this->xmlString .= '<urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd http://www.google.com/schemas/sitemap-image/1.1 http://www.google.com/schemas/sitemap-image/1.1/sitemap-image.xsd" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
         $this->xmlString .= '#contents';
         $this->xmlString .= '</urlset>';
     }
@@ -130,7 +146,7 @@ class SitemapConfigurator
      */
     private function addMergeXmlHead() {
         $this->xmlString = '<?xml version="1.0" encoding="utf-8" ?>';
-        $this->xmlString .= '<sitemapindex xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/siteindex.xsd">';
+        $this->xmlString .= '<urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd http://www.google.com/schemas/sitemap-image/1.1 http://www.google.com/schemas/sitemap-image/1.1/sitemap-image.xsd" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
         $this->xmlString .= '#contents';
         $this->xmlString .= '</sitemapindex>';
     }
